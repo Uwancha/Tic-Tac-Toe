1698536ac7bca9997094584c8812178603fbd45c
Uwancha <sito2alemu@gmail.com>
2023-06-29 12:59:37 +0300

    feat: Implement game board, display controller, and player objects
    This commit adds the game board, display controller, and player objects to the codebase using the module pattern and factory function. The game board is represented as an array of strings, the display controller prints the board to the console in a readable format, and the player objects have a playerMark method that returns their mark. The code uses let and const keywords to declare variables and is well-organized and easy to follow. No additional functionality has been added yet.

---
+++ main.js
@@ -0,0 +1,46 @@
+const gameBoard = (function() {
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    return {board};
+})()
+
+const displayController = function() {
+    console.log(` ${gameBoard.board[0]} | ${gameBoard.board[1]} | ${gameBoard.board[2]} `)
+    console.log("---+---+---");
+    console.log(` ${gameBoard.board[3]} | ${gameBoard.board[4]} | ${gameBoard.board[5]} `)
+    console.log("---+---+---");
+    console.log(` ${gameBoard.board[6]} | ${gameBoard.board[7]} | ${gameBoard.board[8]} `)
+    console.log("---+---+---");
+
+}
+
+const Players = function() {
+    function playerMark(mark) {
+        return mark;
+    }
+
+    return {playerMark};
+}
+
+const player = Players();
+
+const player1 = player.playerMark("X");
+const player2 = player.playerMark("O");
+
+const playersMove = (index) => {
+    if (gameBoard.board[index] === "") {
+    gameBoard.board[index] = currentPlayer;
+    displayController();
+    }
+    switchPlayer()
+}
+
+let currentPlayer = player1
+
+const switchPlayer = () => {
+    currentPlayer = player1 ? player2 : player1
+}
+
+
+
+
