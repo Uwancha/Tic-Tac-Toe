6a507e4f5c603d1170bb7f3ca18eef99b6d91bfc
Uwancha <sito2alemu@gmail.com>
2023-06-30 17:19:11 +0300

    refactor: restructure code into separate modules
    - Create modules for game board, display controller, player, and game logic
    - Implement module pattern to encapsulate game functionality
    - Improve code structure for easier maintenance and updates

--- main.js
+++ main.js
@@ -1,47 +1,131 @@
 const gameBoard = (function() {
     let board = ["", "", "", "", "", "", "", "", ""];
 
-    return {board};
+    const getBoard = () => board;
+
+    const setMark = (index, mark) => {
+        if (board[index] === "") {
+            board[index] = mark;
+            return true
+        }
+        return false
+    } 
+
+    return {getBoard, setMark};
 })()
 
-const displayController = function() {
-    console.log(` ${gameBoard.board[0]} | ${gameBoard.board[1]} | ${gameBoard.board[2]} `)
-    console.log("---+---+---");
-    console.log(` ${gameBoard.board[3]} | ${gameBoard.board[4]} | ${gameBoard.board[5]} `)
-    console.log("---+---+---");
-    console.log(` ${gameBoard.board[6]} | ${gameBoard.board[7]} | ${gameBoard.board[8]} `)
-    console.log("---+---+---");
+const displayController = (function() {
 
-}
-
-const Players = () => {
-    const playerMark = (mark) => {
-        return mark;
+    const renderGameBoard = () => {
+        const board = gameBoard.getBoard()
+        console.log(` ${board[0]} | ${board[1]} | ${board[2]} `)
+        console.log("---------");
+        console.log(` ${board[3]} | ${board[4]} | ${board[5]} `)
+        console.log("----------");
+        console.log(` ${board[6]} | ${board[7]} | ${board[8]} `)
+        console.log("---------");
     }
 
-    return {playerMark};
+    return {renderGameBoard}
+})()
+
+const Players = (mark) => {
+    const getMark = () => mark;
+
+    return {getMark};
 }
 
-const player = Players();
 
-const player1 = player.playerMark("X");
-const player2 = player.playerMark("O");
+const player1 = Players("X");
+const player2 = Players("O");
 
-let currentPlayer = player1
 
-const playersMove = (index) => {
-    if (gameBoard.board[index] === "") {
-    gameBoard.board[index] = currentPlayer;
-    displayController();
+
+const gameLogic = (function() {
+    const _checkForWin = (gameBoard, currentPlayer) => {
+        const winingConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+    
+    
+        for (let i = 0; i < winingConditions.length; i++) {
+            const combinations = winingConditions[i]
+    
+            const isWin = combinations.every(
+                (index) => gameBoard[index] === currentPlayer.getMark()
+            )
+    
+            if (isWin) {
+                console.log(`${currentPlayer.getMark()} wins!`)
+                return true;
+            }
+        }
+        return false
     }
-    switchPlayer()
-}
+    
+    const _checkForTie = (gameBoard) => {
+        if (_checkForWin(gameBoard, currentPlayer)) {
+            return false
+        }
+    
+        for (let i = 0; i < gameBoard.length; i++) {
+            if(gameBoard[i] === "") {
+                return false
+            }
+        }
+    
+        return true;
+    }
+    
+    const _restartTheGame = () => {
 
+        for (let i = 0; i < gameBoard.getBoard().length; i++){
+            gameBoard.setMark(i, "")
+        }
+    }
+    
+    
+    const playGame = (index, gamePlayer) => {
+        if (gameBoard.setMark(index, gamePlayer.getMark())) {
+            displayController.renderGameBoard();
+              
+            if (_checkForWin(gameBoard.getBoard(), gamePlayer)) {
+                console.log(`${gamePlayer.getMark} wins!`)
+                _restartTheGame();
+            }
+    
+            else if (_checkForTie(gameBoard.getBoard())) {
+                console.log(`It is a tie`)
+                _restartTheGame();
+            }
+            switchPlayer()
+            
+        
+        }
+        
+        
+    }
+
+    
+    return { playGame };
+    
+    
+})()
 
 const switchPlayer = () => {
     currentPlayer = currentPlayer === player1 ? player2 : player1
 }
 
+let currentPlayer = player1;
 
+displayController.renderGameBoard();
 
+gameLogic.playGame(0, currentPlayer);
 
