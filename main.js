227f1aa3672ed79205c332b5f3aeeada76f2f287
Uwancha <sito2alemu@gmail.com>
2023-07-02 21:58:59 +0300

    feat: Integrate UI with game logic and add animations
    - Connect game board buttons with game logic functions
    - Display current player's turn and winner in result display
    - Add CSS animations to welcome message and result display
    - Improve code structure and naming conventions

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const result = document.querySelector(".result")
+const resRestart = document.querySelector(".result-restart")
+const msg = document.querySelector(".start")
+const btn = document.querySelector("#btn")
+const header = document.querySelector("header")
+const footer = document.querySelector("footer")
+const main = document.querySelector(".game-board")
+
+
 const gameBoard = (function() {
     let board = ["", "", "", "", "", "", "", "", ""];
 
@@ -31,15 +40,15 @@ const displayController = (function() {
 
 const Players = (mark) => {
     const getMark = () => mark;
-
-    return {getMark};
+    const score = 0
+    return {getMark, score};
 }
 
 
 const player1 = Players("X");
 const player2 = Players("O");
 
-
+let currentPlayer = player1;
 
 const gameLogic = (function() {
     const _checkForWin = (gameBoard, currentPlayer) => {
@@ -84,37 +93,48 @@ const gameLogic = (function() {
         return true;
     }
     
-    const _restartTheGame = () => {
-
-        for (let i = 0; i < gameBoard.getBoard().length; i++){
-            gameBoard.setMark(i, "")
+    const restartTheGame = (arg1, arg2, arg3) => {
+        for (let i = 0; i < arg1.getBoard().length; i++){
+            arg1.getBoard()[i] = ""
         }
+        arg2.classList.remove("show")
+        arg3.forEach((arg) => arg.textContent = "")
+        console.log(`Restart`)
+        if (true) {
+            currentPlayer = player1
+            console.log(`${currentPlayer.getMark()}`)
+            console.log(`yes`)
+            turnDisplay.textContent = `${currentPlayer.getMark()}'s turn`
+            }
     }
     
     
-    const playGame = (index, gamePlayer) => {
+    const playGame = (index, gamePlayer, disp) => {
         if (gameBoard.setMark(index, gamePlayer.getMark())) {
             displayController.renderGameBoard();
               
             if (_checkForWin(gameBoard.getBoard(), gamePlayer)) {
-                console.log(`${gamePlayer.getMark} wins!`)
-                _restartTheGame();
+                console.log(`${gamePlayer.getMark()} wins!`)
+                gamePlayer.score += 1
+                disp.textContent = `${gamePlayer.getMark()} wins!`;
+                resRestart.classList.add("show")
+                
             }
     
             else if (_checkForTie(gameBoard.getBoard())) {
                 console.log(`It is a tie`)
-                _restartTheGame();
+                disp.textContent = `It's a tie!`
+                resRestart.classList.add("show")
             }
             switchPlayer()
             
-        
         }
         
         
     }
 
     
-    return { playGame };
+    return { playGame, restartTheGame };
     
     
 })()
@@ -123,9 +143,49 @@ const switchPlayer = () => {
     currentPlayer = currentPlayer === player1 ? player2 : player1
 }
 
-let currentPlayer = player1;
+//let currentPlayer = player1;
 
 displayController.renderGameBoard();
 
-gameLogic.playGame(0, currentPlayer);
+//gameLogic.playGame(0, currentPlayer);
+
+const cells = document.querySelectorAll(".cell")
+const turnDisplay = document.querySelector("#turn-display")
+const restart = document.querySelector("button")
+
+//turnDisplay.textContent = `${currentPlayer.getMark()}'s turn`
+
+cells.forEach((cell, index) => {
+    cell.addEventListener("click", () => {
+        if (cell.textContent === "") {
+            cell.textContent = currentPlayer.getMark()
+            gameLogic.playGame(index, currentPlayer, result);
+            turnDisplay.textContent = `${currentPlayer.getMark()}'s turn`
+        }
+    });
+});
+
+restart.addEventListener("click", () => {
+    gameLogic.restartTheGame(gameBoard, resRestart, cells);
+    displayController.renderGameBoard();
+ }
+)
+
+window.addEventListener("load", () => {
+    msg.classList.add("anime")
+    main.classList.add("msg")
+    header.classList.add("msg")
+    footer.classList.add("msg")
+})
+
+btn.addEventListener("click", () => {
+    msg.classList.add("msg");
+    msg.classList.remove("anime")
+    main.classList.remove("msg")
+    header.classList.remove("msg")
+    footer.classList.remove("msg")
+})
+
+turnDisplay.textContent = `${currentPlayer.getMark()}'s turn`
+
 
